04b32b44da74c01cc5fc80458b76fd8d243200a0
onursercanyilmaz <onursercanyilmaz@gmail.com>
2021-10-30 17:00:34 +0300

    Skeleton Ready
    Syntax will be highlighted layer.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,15 @@ module org.thehive.hivedesktop {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires javafx.graphics;
+    requires javafx.base;
+
+    requires java.sql;
+    requires java.datatransfer;
+    requires java.desktop;
+    requires java.compiler;
+
+
 
     opens org.thehive.hivedesktop to javafx.fxml;
     exports org.thehive.hivedesktop;
