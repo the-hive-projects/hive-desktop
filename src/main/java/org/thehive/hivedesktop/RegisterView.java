51fb162a43db965c0a9e57af7e87609a90006dbf
Mustafa Tüfekçi <tufekci.mustafa@std.izu.edu.tr>
2021-11-02 19:26:13 +0300

    nitial commit

--- src/main/java/org/thehive/hivedesktop/RegisterView.java
+++ src/main/java/org/thehive/hivedesktop/RegisterView.java
@@ -15,7 +15,7 @@ public class RegisterView extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SessionView.class.getResource("register-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.getIcons().add(new Image(InboxView.class.getResourceAsStream("/img/logo2.png")));
         stage.setTitle("Hello!");
 
