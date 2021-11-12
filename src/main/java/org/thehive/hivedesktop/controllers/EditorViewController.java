93614e6e5bf9bf4cf1ede1c803a9cf0acf7d2a2a
onursercanyilmaz <onursercanyilmaz@gmail.com>
2021-11-12 14:13:04 +0300

    Editor and Terminal Connected
    Terminal can execute python code.

--- src/main/java/org/thehive/hivedesktop/controllers/EditorViewController.java
+++ src/main/java/org/thehive/hivedesktop/controllers/EditorViewController.java
@@ -3,27 +3,30 @@ package org.thehive.hivedesktop.controllers;
 import com.kodedu.terminalfx.TerminalBuilder;
 import com.kodedu.terminalfx.TerminalTab;
 import com.kodedu.terminalfx.config.TerminalConfig;
+import eu.mihosoft.monacofx.Document;
 import eu.mihosoft.monacofx.MonacoFX;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
-import org.thehive.hivedesktop.EditorView;
 
-import java.io.IOException;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
 import java.net.URL;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 
 public class EditorViewController implements Initializable {
 
     @FXML
-    MFXButton runCode;
+    MFXButton btnRunCode;
 
     @FXML
     MFXButton btnAddNewTab;
@@ -39,46 +42,106 @@ public class EditorViewController implements Initializable {
 
 
 
+    Dictionary<String, MonacoFX> dict
+            = new Hashtable<String, MonacoFX>();
+
+
     @FXML
     private MonacoFX setEditor(String language,String theme)
     {
         MonacoFX monacoFXeditor = new MonacoFX();
-        int numTabs = editorPane.getTabs().size();
-        monacoFXeditor.setId(String.valueOf(numTabs));
+        int numTabs = dict.size();
+        monacoFXeditor.setId("monacoFX"+String.valueOf(numTabs));
         monacoFXeditor.getEditor().getDocument().setText(
-                "#include <stdio.h>\n" +
-                        "int main() {\n" +
-                        "   // printf() displays the string inside quotation\n" +
-                        "   printf(\"Hello, World!\");\n" +
-                        "   return 0;\n" +
-                        "}");
+                "num = float(input(\"Enter a number: \"))\r" +
+                        "if num > 0:\n" +
+                        "   print(\"Positive number\")\n" +
+                        "elif num == 0:\n" +
+                        "   print(\"Zero\")\n" +
+                        "else:\n" +
+                        "   print(\"Negative number\")\n");
 
 
         // use a predefined language like 'c'
         monacoFXeditor.getEditor().setCurrentLanguage(language);
         monacoFXeditor.getEditor().setCurrentTheme(theme);
-        System.out.println(monacoFXeditor.getId());
+       //System.out.println(monacoFXeditor.getId());
 
         return monacoFXeditor;
     }
 
     @FXML
-    private String addTab() throws IOException {
-        int numTabs = editorPane.getTabs().size();
+    private MonacoFX addTab() throws IOException {
+        int numTabs = dict.size();
         Tab tab = new Tab("Tab "+ numTabs);
         tab.setId(String.valueOf(numTabs));
-        System.out.println(tab.getId());
-        tab.setContent(setEditor("python","vs-dark"));
+        //System.out.println(tab.getId());
+        var settedEditor = setEditor("python","vs-dark");
+        tab.setContent(settedEditor);
+        addToDict(tab.getText(),settedEditor);
         editorPane.getTabs().add(tab);
 
-        return tab.getId();
+        return settedEditor;
+    }
+
+    @FXML
+    private void addToDict(String tabName, MonacoFX editorName)
+    {
+
+
+        // Inserting values into the Dictionary
+        dict.put(tabName, editorName);
+        System.out.println(dict);
+
+
+    }
+
+
+    @FXML
+    private static void readFile1(File fin) throws IOException {
+        FileInputStream fis = new FileInputStream(fin);
+
+        //Construct BufferedReader from InputStreamReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        br.close();
+    }
+
+
+    @FXML
+    private File runEditorCode(TerminalTab terminal) throws IOException {
+        var currentTabName = editorPane.getSelectionModel().getSelectedItem().getText();
+        MonacoFX currentEditor = dict.get(currentTabName);
+        //System.out.println(currentEditor.getId());
+
+        var currentEditorContent = currentEditor.getEditor().getDocument().getText();
+        System.out.println(currentEditorContent);
+
+        var codeDoc = currentEditor.getEditor().getDocument();
+        codeDoc.setLanguage("python");
+
+        File fout = new File("out.py");
+        FileOutputStream fos = new FileOutputStream(fout);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+        bw.write(codeDoc.getText());
+        bw.close();
+
+
+
+        terminal.getTerminal().command("python "+fout+"\r");
+
+        return fout;
+
+
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
-
-
 
 
         btnAddNewTab.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -101,6 +164,7 @@ public class EditorViewController implements Initializable {
         darkConfig.setForegroundColor(Color.rgb(240, 240, 240));
         darkConfig.setCursorColor(Color.web("#ffc107"));
 
+
 //        CygWin Config
         TerminalConfig cygwinConfig = new TerminalConfig();
         cygwinConfig.setWindowsTerminalStarter("C:\\cygwin64\\bin\\bash -i");
@@ -117,6 +181,21 @@ public class EditorViewController implements Initializable {
 //            terminal.getTerminal().command("java -version\r");
 //        });
 
+
+
+
+        btnRunCode.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+
+                try {
+                    readFile1(runEditorCode(terminal));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
         terminalPane.getTabs().add(terminal);
 
 
